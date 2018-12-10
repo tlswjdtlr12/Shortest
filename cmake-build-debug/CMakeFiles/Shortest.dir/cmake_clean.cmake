2fa5f615969e1a27ba9903eb27ee4eb6fa0ca246
2014004739 <2014004739@hanyang.ac.kr>
2018-12-10 10:26:34 +0900

    first

---
+++ cmake-build-debug/CMakeFiles/Shortest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shortest.dir/main.c.o"
+  "Shortest.pdb"
+  "Shortest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Shortest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
